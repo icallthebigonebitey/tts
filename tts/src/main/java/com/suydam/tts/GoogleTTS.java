ba8be1dcb03902f1909bd56030e6aa41f28b12fe
icallthebigonebitey <orion.suydam@gmail.com>
2022-09-02 15:50:33 -0500

    Support both MP3 and streaming WAV file

--- tts/src/main/java/com/suydam/tts/GoogleTTS.java
+++ tts/src/main/java/com/suydam/tts/GoogleTTS.java
@@ -2,6 +2,17 @@ package com.suydam.tts;
 
 import org.apache.commons.cli.*;
 
+import javax.sound.sampled.*;
+
+/* requires google-cloud-pubsub dependency in pom.xml
+ * https://cloud.google.com/java/docs/reference/google-cloud-pubsub/latest/com.google.pubsub.v1.ProjectSubscriptionName
+ * https://cloud.google.com/java/docs/reference/google-cloud-pubsub/latest/com.google.cloud.pubsub.v1.SubscriptionAdminClient
+ * Would also need to create a Topic on project and a subscription for that Topic to run the code below
+ * */
+//import com.google.pubsub.v1.*;
+//import com.google.cloud.pubsub.v1.*;
+
+
 //Imports the Google Cloud client library
 import com.google.cloud.texttospeech.v1.AudioConfig;
 import com.google.cloud.texttospeech.v1.AudioEncoding;
@@ -11,23 +22,28 @@ import com.google.cloud.texttospeech.v1.SynthesizeSpeechResponse;
 import com.google.cloud.texttospeech.v1.TextToSpeechClient;
 import com.google.cloud.texttospeech.v1.VoiceSelectionParams;
 import com.google.protobuf.ByteString;
+//import com.google.cloud.internal.PickFirstLoadBalancer;
 
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.OutputStream;
+import java.io.InputStream;
 
 /**
  * Hello world!
  *
  */
-public class GoogleTTS 
+public class GoogleTTS implements LineListener
 {
 	private TextToSpeechClient textToSpeechClient;
 	private AudioConfig audioConfig;
+	boolean playCompleted;
 	
     public static void main( String[] args )
     {
-    	String verbiage = "ม";
+    	String verbiage = "งาน";
+    	// งาน  ม
+    	// "ในวันฝนพรำเธอคิดถึงกันบ้างไหมในตอนที่ไม่ได้เจอแล้วเธอนั้นเป็นอย่างไร"
     	String language = "th-TH";
     	
     	// define options
@@ -62,11 +78,21 @@ public class GoogleTTS
         System.out.println("Verbalizing: " + verbiage);
         System.out.println("Language Code: " + language);
         
+        /*
+        ProjectSubscriptionName subscriptionName = ProjectSubscriptionName.of("TTS Quickstart", subscriptionId);
+        try (SubscriptionAdminClient subscriptionAdminClient = SubscriptionAdminClient.create()) {
+            subscriptionAdminClient.getSubscription(subscriptionName); // Exception is thrown in this line
+        } catch (Exception t) {
+        	t.printStackTrace();
+        	System.exit(1);
+        }
+        */
+        
         try {
         	GoogleTTS client = new GoogleTTS();
-        	String resource = client.renderAsMP3(verbiage, language);
-        	
-        } catch (IOException e) {
+        	//String resource = client.renderAsMP3(verbiage, language);
+        	client.playAsWAV(verbiage, language);
+        } catch (Exception e) {
         	e.printStackTrace();
         	System.exit(1);
         }
@@ -74,14 +100,15 @@ public class GoogleTTS
         System.exit(0);
     }
     
+    
     public GoogleTTS() throws IOException {
+    	//LoadBalancerRegistry.getDefaultRegistry().register(new PickFirstLoadBalancerProvider());
     	textToSpeechClient = TextToSpeechClient.create();
-    	
-    	// Select the type of audio file you want returned
-    	audioConfig = AudioConfig.newBuilder().setAudioEncoding(AudioEncoding.MP3).build();
     }
     
-    public String renderAsMP3(String verbiage, String language) {
+    
+    private ByteString synthesize(String verbiage, String language) {
+ 
     	// Set the text input to be synthesized
     	SynthesisInput input = SynthesisInput.newBuilder().setText(verbiage).build();
     	
@@ -95,9 +122,16 @@ public class GoogleTTS
     	SynthesizeSpeechResponse response = textToSpeechClient.synthesizeSpeech(input, voice, audioConfig);
     	
     	// Get the audio contents from the response
-    	ByteString audioContents = response.getAudioContent();
-
-    	   
+    	return response.getAudioContent();
+    }
+    
+    public String renderAsMP3(String verbiage, String language) {
+    	// Select the type of audio file you want returned
+    	// https://cloud.google.com/text-to-speech/docs/reference/rpc/google.cloud.texttospeech.v1#google.cloud.texttospeech.v1.AudioEncoding
+    	audioConfig = AudioConfig.newBuilder().setAudioEncoding(AudioEncoding.MP3).build();
+    	
+		ByteString audioContents = synthesize(verbiage, language);
+		
     	// Write the response to the output file.
     	String fileName = "output.mp3";
     	try (OutputStream out = new FileOutputStream(fileName)) {
@@ -110,6 +144,52 @@ public class GoogleTTS
     	}
     	
     	return fileName;
+    }
+    
+    public void playAsWAV(String verbiage, String language) {
+    	// Select the type of audio file you want returned
+    	// https://cloud.google.com/text-to-speech/docs/reference/rpc/google.cloud.texttospeech.v1#google.cloud.texttospeech.v1.AudioEncoding
+    	audioConfig = AudioConfig.newBuilder().setAudioEncoding(AudioEncoding.LINEAR16).build();
+    	
+    	ByteString audioContents = synthesize(verbiage, language);
+    	InputStream inputStream = audioContents.newInput();
+    	
+    	try {
+	    	AudioInputStream audioStream = AudioSystem.getAudioInputStream(inputStream);
+	    	
+	    	AudioFormat audioFormat = audioStream.getFormat();
+	    	DataLine.Info info = new DataLine.Info(Clip.class, audioFormat);
+	    	
+	    	Clip audioClip = (Clip) AudioSystem.getLine(info);
+	    	audioClip.addLineListener(this);
+	    	audioClip.open(audioStream);
+	    	audioClip.start();
+	    	
+	    	while (!playCompleted) {
+                // wait for the playback completes
+                try {
+                    Thread.sleep(1000);
+                } catch (InterruptedException ex) {
+                    ex.printStackTrace();
+                }
+            }
+             
+            audioClip.close();
+            audioStream.close();
+    	} catch (Exception e) {
+    		e.printStackTrace();
+    		System.exit(1);
+    	}
+    	
+    }
+    
+    public void update(LineEvent event) {
+        LineEvent.Type type = event.getType();
+         
+        if (type == LineEvent.Type.STOP) {
+            playCompleted = true;
+            System.out.println("Playback completed.");
+        }
     }
 }
 
